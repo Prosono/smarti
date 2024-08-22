beb3d5290cbc26431590fb55d7a4697ae550777c
Prosono <norman576@hotmail.com>
2024-08-22 17:12:32 +0200

    1.3.18

--- custom_components/smartiupdater/updater.py
+++ custom_components/smartiupdater/updater.py
@@ -244,9 +244,10 @@ async def merge_strømpriser_flow(session: aiohttp.ClientSession):
                     updated_flows.append(strømpriser_flow)
 
                 # Save the merged flows.json back to the file
-                async with aiofiles.open(strømpriser_file_url, 'w') as file:
+                async with aiofiles.open(strømpriser_file_url, 'w', encoding='utf-8') as file:
                     await file.write(json.dumps(updated_flows, indent=4))
                     _LOGGER.info(f"Merged strømpriser flow successfully into {strømpriser_file_url}.")
+                    _LOGGER.debug(f"Final updated flows content: {json.dumps(updated_flows, indent=4)}")
     except Exception as e:
         _LOGGER.error(f"Error merging strømpriser flow: {str(e)}")
 
